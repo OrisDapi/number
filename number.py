ec1c63d307607431aa16ae983ad5efd2294777b8
Oris Chen <orispont@gmail.com>
2021-01-02 16:59:30 +0800

    final version

--- number.py
+++ number.py
@@ -1,24 +1,27 @@
 import random
-num = random.randint(1, 100)
+
+start = input('請輸入起點值: ')
+end = input('請輸入終點值: ')
+start = int(start)
+end = int(end)
+
+num = random.randint(start, end)
 num = int(num)
 
 count = 0
 
 while True :
     count = count + 1 #count += 1
-    num1 = input('1~100內請猜一數字')
+    num1 = input('請猜一數字')
     num1 = int(num1)
     if num1 == num :
         print('恭喜猜中！')
         print('這是你猜的第', count, '次')
         break
-        
+
     elif num1 > num :
         print('smaller than you guess')
     elif num1 < num :
         print('bigger than you guess')
 
     print('這是你猜的第', count, '次')
-
-if num1 < '1' and num1 > '100' :
-    print('請輸入1～100數字')
